c31f0103563cc62586c39fddb2bcb50217693872
hoonjo123 <johoon030@gmail.com>
2024-02-08 22:10:58 +0900

    final

--- src/views/MemberOrders.vue
+++ src/views/MemberOrders.vue
@@ -1,20 +1,13 @@
 <template>
-        <order-list-component
-    :isAdmin="true"
-    :apiUrl="`http://localhost:8080/member/${id}/orders`"
-    />
-</template>
-
-<script>
-
-import OrderListComponent from '@/components/OrderListComponent.vue'
-
-export default {
-    props:['id'],
-    components:{
-        OrderListComponent
+    <OrderListComponent :isAdmin="true" :apiUrl="`/member/${id}/orders`" />
+  </template>
+  
+  <script>
+  import OrderListComponent from "@/components/OrderListComponent.vue";
+  export default {
+    props: ["id"],
+    components: {
+      OrderListComponent,
     },
-    //매개변수로 데이터 넘겨받을 때에 props를 사용한다.
-    
-}
-</script>
+  };
+  </script>
--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,18 +1,11 @@
 <template>
-    <order-list-component
-    :isAdmin="true"
-    apiUrl="http://localhost:8080/orders"
-    />
-
-</template>
-
-<script>
-import OrderListComponent from '@/components/OrderListComponent.vue'
-
-export default {
-    components:{
-        OrderListComponent
-    }
-        
-}
-</script>
+    <OrderListComponent :isAdmin="true" apiUrl="/orders" />
+  </template>
+  <script>
+  import OrderListComponent from "@/components/OrderListComponent.vue";
+  export default {
+    components: {
+      OrderListComponent,
+    },
+  };
+  </script>
